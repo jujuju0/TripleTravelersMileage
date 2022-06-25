3edd88daa618144fa7aaeb6171653db64b6295b6
jujuju0 <ljy000716@gmail.com>
2022-06-25 15:32:24 +0900

    Initial commit

---
+++ src/main/java/pretask/triple/mileage/TripleTravelersMileage/TripleTravelersMileageApplication.java
@@ -0,0 +1,13 @@
+package pretask.triple.mileage.TripleTravelersMileage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TripleTravelersMileageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TripleTravelersMileageApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pretask/triple/mileage/TripleTravelersMileage/TripleTravelersMileageApplicationTests.java
@@ -0,0 +1,13 @@
+package pretask.triple.mileage.TripleTravelersMileage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TripleTravelersMileageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
