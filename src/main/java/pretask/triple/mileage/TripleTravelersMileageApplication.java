32280af17fb8a8b8bc63cfd4ea05a93ffee3a392
jujuju0 <ljy000716@gmail.com>
2022-06-26 20:33:49 +0900

    api 서버 구축 mysql, jpa 연동 리뷰 이벤트, 포인트 조회 기능 개발

--- src/main/java/pretask/triple/mileage/TripleTravelersMileageApplication.java
+++ src/main/java/pretask/triple/mileage/TripleTravelersMileageApplication.java
@@ -1,4 +1,4 @@
-package pretask.triple.mileage.TripleTravelersMileage;
+package pretask.triple.mileage;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/pretask/triple/mileage/TripleTravelersMileage/TripleTravelersMileageApplicationTests.java
+++ src/test/java/pretask/triple/mileage/TripleTravelersMileage/TripleTravelersMileageApplicationTests.java
@@ -1,9 +1,8 @@
 package pretask.triple.mileage.TripleTravelersMileage;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class TripleTravelersMileageApplicationTests {
 
 	@Test
